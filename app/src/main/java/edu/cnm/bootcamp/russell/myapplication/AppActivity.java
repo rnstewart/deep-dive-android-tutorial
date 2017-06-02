9759d4fa91f57f1b1dad99c73f2ee55f8f3d1937
Russell Stewart <rnstewart@gmail.com>
2017-06-01 18:30:41 -0600

    Adding a SearchView to the toolbar, and updating AppActivity to receive data from the SearchView.

--- app/src/main/java/edu/cnm/bootcamp/russell/myapplication/AppActivity.java
+++ app/src/main/java/edu/cnm/bootcamp/russell/myapplication/AppActivity.java
@@ -1,9 +1,14 @@
 package edu.cnm.bootcamp.russell.myapplication;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
 
 import edu.cnm.bootcamp.russell.myapplication.fragments.ListFragment;
 
@@ -15,6 +20,26 @@ public class AppActivity extends AppCompatActivity implements ListFragment.OnFra
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
         loadFragment();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_app_activity, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName())
+        );
+
+        return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_SEARCH)) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+        }
     }
 
     private void loadFragment() {
